5938b8b6d55df7b2342cc269145a8fdce164c205
Elg-Mouad <melgoual@moenn-technologies.com>
2025-02-26 21:59:57 +0000

    V1.1

--- Backend/app.js
+++ Backend/app.js
@@ -14,12 +14,12 @@ import control from './routes/control.route.js';
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 app.use(cors());
 app.use(cors({
-     origin: '*',
-     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-     credentials: true,
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
 }));
 
 app.use(express.json())
@@ -37,10 +37,10 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, async () => {
-    try{
+    try {
         connectSQL();
         console.log(`Server is running on ${port}`)
-    }catch(error) {
+    } catch (error) {
         console.log('Error while starting the server: ', error)
     }
 })
